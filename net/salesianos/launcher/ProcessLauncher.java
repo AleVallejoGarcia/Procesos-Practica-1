c0b6a9abb4361ef74805c36af7f99b3ddf797be9
AleVallejoGarcia <alevalga@gmail.com>
2023-10-11 10:07:39 +0100

    feat:(Creación ProcessLauncher.java)

---
+++ net/salesianos/launcher/ProcessLauncher.java
@@ -0,0 +1,26 @@
+package salesianos.launcher;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ProcessLauncher {
+    private final static String OUTPUT_ROUTE = "./net/salesianos/outputs/";
+
+    public static Process initVocalCounterProcess(String line, String fileName) {
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "java", "./net/salesianos/processes/VocalCounter.java", line
+        );
+
+        Process javaProcess = null;
+
+        try {
+            File outputFile = new File(OUTPUT_ROUTE + fileName);
+            processBuilder.redirectOutput(outputFile);
+            javaProcess =  processBuilder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return javaProcess;
+    }
+}
